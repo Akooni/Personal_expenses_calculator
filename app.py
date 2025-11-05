cad3d56c12c3783fdcd4cac8685994e8d5428e35
Akooni <127095348+Akooni@users.noreply.github.com>
2025-11-05 14:12:10 +0400

    add a loop to make the user only choose a or q

--- app.py
+++ app.py
@@ -6,23 +6,28 @@ from datetime import date
 today = str(date.today())
 while True:
     choice = input("Add expense or quit? (a/q): ").lower()
+    if choice == "a":
+        date = input("Enter Date (YYYY-MM-DD):")
+        if date == "":
+            date = today
+
+        category= input("Enter Category:")
+        amount= float(input("Enter the amount here:"))
+        desc = str(input("Enter description: "))
+        with open("expenses.csv",'a') as f:
+            f.write(f"{date},{category},{amount},{desc}\n")
+            print("✅ Expense added!")
+        df = pd.read_csv("expenses.csv")
+        totals = df.groupby("category")["amount"].sum()
+        print("\n Totals so far: \n",totals)
+
     if choice == "q":
         break
 
-    date = input("Enter Date (YYYY-MM-DD):")
-    if date == "":
-        date = today
+    else:
+        print("Please Enter a number here not a Text :) ")
 
-    category= input("Enter Category:")
-    amount= float(input("Enter the amount here:"))
-    desc = str(input("Enter description: "))
-    with open("expenses.csv",'a') as f:
-        f.write(f"{date},{category},{amount},{desc}\n")
-        print("✅ Expense added!")
-    df = pd.read_csv("expenses.csv")
-    totals = df.groupby("category")["amount"].sum()
-    print("\n Totals so far: \n",totals)
-
+   
 
 
 
